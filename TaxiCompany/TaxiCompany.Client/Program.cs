cd2dd6fcb7c998df488d7dfa4f88bb0daa7f8d3e
Степан Семыкин <semykinstepan2@gmail.com>
2024-12-12 21:20:29 +0400

    Удаление лишнего кода в program.cs

--- TaxiCompany/TaxiCompany.Client/Program.cs
+++ TaxiCompany/TaxiCompany.Client/Program.cs
@@ -1,59 +1,8 @@
 using Blazorise;
 using Blazorise.Bootstrap5;
 using Blazorise.Icons.FontAwesome;
-using System.Net.NetworkInformation;
-using TaxiCompany.Client.Components;
-using TaxiCompany.WebApi;
-using TaxiCompany.Domain.Entities;
-using MudBlazor;
 using MudBlazor.Services;
-using Microsoft.AspNetCore.Components.Web;
-using System.Net.Http;
-using Microsoft.AspNetCore.Builder;
-
-//var builder = WebApplication.CreateBuilder(args);
-
-//// Add services to the container.
-//builder.Services.AddRazorComponents()
-//    .AddInteractiveServerComponents();
-
-//builder.Services.AddScoped<TaxiCompanyApiWrapper>();
-//// builder.Services.AddSingleton<ITaxiCompanyApiWrapper, TaxiCompanyApiWrapper>();
-//builder.Services.AddMudServices();
-
-//builder.Services.AddHttpClient(); // 
-//builder.Services.AddHttpContextAccessor(); //
-//builder.Services.AddScoped(sp =>
-//    new HttpClient
-//    {
-//        BaseAddress = new Uri(builder.Configuration["BasePath"]!)
-//    }); //
-//Console.WriteLine($"BasePath: {builder.Configuration["BasePath"]}");
-
-//builder.Services
-//    .AddBlazorise(options =>
-//    {
-//        options.Immediate = true;
-//    })
-//    .AddBootstrap5Providers()
-//    .AddFontAwesomeIcons();
-
-//var app = builder.Build();
-
-//// Configure the HTTP request pipeline.
-//if (!app.Environment.IsDevelopment())
-//{
-//    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-//}
-
-//app.UseStaticFiles();
-//app.UseAntiforgery();
-
-//app.MapRazorComponents<App>()
-//    .AddInteractiveServerRenderMode();
-
-//app.Run();
-
+using TaxiCompany.Client.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -63,28 +12,15 @@ builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 builder.Services.AddMudServices();
 
-
 builder.Services.AddScoped(sp =>
-    new HttpClient
-    {
-        BaseAddress = new Uri(builder.Configuration["BasePath"]!)
-    });
+new HttpClient
+{
+    BaseAddress = new Uri(builder.Configuration["BasePath"]!)
+});
 
 builder.Services.AddHttpClient();
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddMudServices();
-//builder.Services.AddMudServices(config =>
-//{
-//    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomLeft;
-
-//    config.SnackbarConfiguration.PreventDuplicates = false;
-//    config.SnackbarConfiguration.NewestOnTop = false;
-//    config.SnackbarConfiguration.ShowCloseIcon = true;
-//    config.SnackbarConfiguration.VisibleStateDuration = 10000;
-//    config.SnackbarConfiguration.HideTransitionDuration = 500;
-//    config.SnackbarConfiguration.ShowTransitionDuration = 500;
-//    config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
-//});
 
 builder.Services
     .AddBlazorise(options =>
@@ -95,7 +31,6 @@ builder.Services
     .AddFontAwesomeIcons();
 
 var app = builder.Build();
-//AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
 if (!app.Environment.IsDevelopment())
 {
@@ -112,6 +47,3 @@ app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
 app.Run();
-
-
-
